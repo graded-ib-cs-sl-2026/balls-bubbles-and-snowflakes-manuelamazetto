09db01ec0a9b315000814af7b3707bd0ed02e19c
Manuela <Nope>
2024-10-30 09:05:11 -0300

    ballsComplete

--- src/Bubble.java
+++ src/Bubble.java
@@ -11,37 +11,42 @@ class Bubble {
         return radius;
     }
 
-    public float getDiameter() {
-        return radius * 2;
-    }
-
-    public float getX() {
-        return x;
-    }
-
-    public float getY() {
-        return y;
-    }
-
-    public void draw() {
-        s.stroke(borderColor);
-        s.fill(fillColor);
-        s.circle(x, y, radius*2);
-    }
-
-    public void move() {
-        x = x + xSpeed;
-        y = y + ySpeed;
-        if (x > s.width - radius) {
-            x = radius;
-        } else if(x < radius) {
-            x = s.width - radius;
-        } else if (y > s.height - radius) {
-            y = radius;
-        } else if (y < radius) {
-            y = s.height - radius;
-        }
-    }
-
-}
+   
  **/
+
+ public float getRadius() {
+    return radius;
+}
+
+public float getDiameter() {
+    return radius * 2;
+}
+
+public float getX() {
+    return x;
+}
+
+public float getY() {
+    return y;
+}
+
+public void draw() {
+    s.stroke(borderColor);
+    s.fill(fillColor);
+    s.circle(x, y, radius*2);
+}
+
+public void move() {
+    x = x + xSpeed;
+    y = y + ySpeed;
+    if (x > s.width - radius) {
+        x = radius;
+    } else if(x < radius) {
+        x = s.width - radius;
+    } else if (y > s.height - radius) {
+        y = radius;
+    } else if (y < radius) {
+        y = s.height - radius;
+    }
+}
+
--- src/Sketch.java
+++ src/Sketch.java
@@ -4,6 +4,8 @@ public class Sketch extends PApplet {
 
     /** Represents one ball */
     private Ball ball1;
+    private Ball ball2;
+    private Ball ball3;
 
     /**
      * This method can only be used to change the window size. It runs before the
@@ -19,6 +21,11 @@ public class Sketch extends PApplet {
      */
     public void setup() {
         ball1 = new Ball(this);
+        ball1.setColors(color(127, 0,0), 0);
+        ball2 = new Ball(this, 20, 30, 450, -3, 3);
+        ball2.setColors(color (159, 121, 212), 0);
+        ball3 = new Ball(this, random(4, 30), random(0, 500), random(0, 500), random (-10, 10), random (-10, 10));
+        ball3.setColors(color (random(0, 255), random(0, 255), random(0, 255)), 0);
         
     }
 
@@ -31,6 +38,10 @@ public class Sketch extends PApplet {
         background(180, 180, 255);
         ball1.draw();
         ball1.move();
+        ball2.draw();
+        ball2.move();
+        ball3.draw();
+        ball3.move();
     }
 
     /** All processing sketches have to use this main method. Don't touch this! */
